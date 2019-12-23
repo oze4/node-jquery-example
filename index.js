3a71fcd38548ba32edca3137292b7d12691623fe
Matt Oestreich <matthewpoestreich@gmail.com>
2019-12-22 19:16:14 -0600

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+const express = require('express');
+const app = express();
+const path = require('path');
+const database = require('./mockDatabase.js');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('port', 8089);
+
+// Route
+app.get('/teachers', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Route 
+app.get('/teachers/:str', (req, res, next) => {
+    try {
+        let id = req.params.str;
+        let teacher = database.find(t => t.id === Number(id)); // Number(id) converts string to int
+        res.status(200).send(teacher);
+    } catch {
+        res.status(500).send("Unable to find that teacher.");
+    }
+});
+
+// 404 route - should be last in list
+app.use((req, res, next) => {
+    res.status(404).send(`Did you mean to browse to <a href='http://localhost:${app.get('port')}/teachers'>'localhost:${app.get('port')}/teachers'</a> ?`);
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`App started on port ${app.get('port')}`)
+});
---
+++ mockDatabase.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        id: 0,
+        name: "Teacher 0",
+        tenure: 6
+    },
+    {
+        id: 1,
+        name: "Teacher 1",
+        tenure: 2
+    },
+    {
+        id: 2,
+        name: "Teacher 2",
+        tenure: 9
+    },
+    {
+        id: 3,
+        name: "Teacher 3",
+        tenure: 3
+    },
+    {
+        id: 4,
+        name: "Teacher 4",
+        tenure: 7
+    },
+    {
+        id: 5,
+        name: "Teacher 5",
+        tenure: 12
+    },
+];
